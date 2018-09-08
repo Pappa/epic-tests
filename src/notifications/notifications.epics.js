97f23bbc6bf8fb188a5a5d300b4a0073cc918147
Pappa <dr.pappa@gmail.com>
2018-09-08 14:28:17 +0100

    asyncScheduler workaround for rxjs bug

--- src/notifications/notifications.epics.js
+++ src/notifications/notifications.epics.js
@@ -1,14 +1,15 @@
 import { LOGIN_SUCCESS, LOGOUT_SUCCESS, onSocketMessageAction, onSocketErrorAction } from './notifications.actions';
 import { ofType } from 'redux-observable';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import { switchMap, map, catchError, takeUntil } from 'rxjs/operators';
 
-export const notificationsEpic = (action$, state$, { fromWebSocket }) => action$.pipe(
-    ofType(LOGIN_SUCCESS),
-    switchMap(() => fromWebSocket()
-        .pipe(
-            map(({ payload }) => onSocketMessageAction(payload)),
-            catchError(e => of(onSocketErrorAction(e)))//,
-            //takeUntil(action$.pipe(ofType(LOGOUT_SUCCESS)))
+export const notificationsEpic = (action$, state$, { fromWebSocket }) => 
+    action$.pipe(
+        ofType(LOGIN_SUCCESS),
+        switchMap(() => fromWebSocket()
+            .pipe(
+                takeUntil(action$.pipe(ofType(LOGOUT_SUCCESS))),
+                map(({ payload }) => onSocketMessageAction(payload)),
+                catchError(e => of(onSocketErrorAction(e)))
+            )
         )
-    )
-);
+    );
--- src/notifications/notifications.epics.test.js
+++ src/notifications/notifications.epics.test.js
@@ -1,21 +1,24 @@
-import { ActionsObservable } from 'redux-observable';
 import { loginSuccessAction, onSocketMessageAction } from './notifications.actions';
 import { notificationsEpic } from './notifications.epics';
 import { toArray } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { asyncScheduler, of } from 'rxjs';
 
 describe('notificationsEpic', () => {
 
     it('should receive a websocket message', done => {
-        const action$ = ActionsObservable.of(loginSuccessAction());
-        const fromWebSocket = of({ payload: 'Hi!' });
+        const action$ = of(loginSuccessAction(), asyncScheduler);
+        const fromWebSocket = () => of({ payload: 'Hi!' });
         const expectedActions = [onSocketMessageAction('Hi!')];
         
         notificationsEpic(action$, {}, { fromWebSocket })
             .pipe(toArray())
             .subscribe(actions => {
-                expect(actions).toEqual(expectedActions);
-                done();
+                try {
+                    expect(actions).toEqual(expectedActions);
+                    done();
+                } catch (e) {
+                    done.fail(e);
+                }
             })
     });
 
