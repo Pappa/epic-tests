af7f3aee21d26d451af8d838a878df65fa267ae1
Pappa <dr.pappa@gmail.com>
2019-02-14 15:23:06 +0000

    redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { compose, createStore, combineReducers, applyMiddleware } from "redux";
+import { createEpicMiddleware } from "redux-observable";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeMiddleware =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({ reducer1: (x = {}) => x });
+const epicMiddleware = createEpicMiddleware({});
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeMiddleware(applyMiddleware(epicMiddleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
